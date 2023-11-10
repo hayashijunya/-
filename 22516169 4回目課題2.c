ed0da1777da30df2a51955a3d0e7e669ddc09471
hayashijunya <148538383+hayashijunya@users.noreply.github.com>
2023-11-10 17:56:16 +0900

    Add files via upload

---
+++ 22516169 4回目課題2.c
@@ -0,0 +1,23 @@
+10月27日
+課題２
+#include<stdio.h>
+
+int main(void)
+{
+
+	int a;
+
+	printf("aの値はいくらですか");
+	scanf_s("%d", &a);
+    
+	if (a % 2 == 0)
+	{
+		printf("aは偶数である");
+	}
+	else
+	{
+		printf("aは奇数です");
+	}
+
+		return 0;
+}
