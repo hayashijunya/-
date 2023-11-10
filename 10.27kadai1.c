cf329f6f2f745eaa0f80bec131d98e2390bae671
hayashijunya <148538383+hayashijunya@users.noreply.github.com>
2023-11-10 19:22:03 +0900

    Add files via upload

---
+++ 10.27kadai1.c
@@ -0,0 +1,22 @@
+10月27日
+課題１
+
+#include<stdio.h>
+
+int main(void)
+{
+
+	int a,b,c;
+
+	printf("aの値はいくらですか？");
+	scanf_s("%d",&a);
+
+	printf("bの値はいくらですか？");
+	scanf_s("%d", &b);
+
+	c = a + b;
+
+    printf("%d",c);
+
+		return 0;
+}
---
+++ 11.10 kadai1.c
@@ -0,0 +1,20 @@
+11月10日
+課題1
+#include<stdio.h>
+
+int main(void)
+{
+	int i, sum = 0;
+
+	int max = 10000;
+
+	for (int i = 1; i <= max; i++)
+	{
+		if (i % 221 == 0)
+		{
+			printf("%d\n", i, sum);
+		}
+	}
+
+	return 0;
+}
