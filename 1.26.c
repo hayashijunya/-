443e78968ec0b37b4861f9cb1972cfec42410450
hayashijunya <148538383+hayashijunya@users.noreply.github.com>
2024-01-26 18:55:05 +0900

    Add files via upload

---
+++ 1.26.c
@@ -0,0 +1,37 @@
+自由課題
+
+#include<stdio.h>
+#include<time.h>
+#include<windows.h>
+
+void main()
+{
+	int n1;
+	int n2;
+	int i;
+	char c[128];
+
+	system("cls");
+
+	srand(time(NULL));
+	n1 = rand() % 100+1;
+
+	printf(" 今コンピューターが数字を一つ選びました　*\n");
+	printf(" 選んだ数字をあててください*\n");
+	printf(" 数字の範囲は0から100です*\n");
+
+	for (i = 1; i <= 10; i++)
+	{
+		printf("\n数字: ");
+		fflush(stdout);
+		gets(c); n2 = atoi(c);
+		if (n1 == n2) break;
+		if (n1 < n2)
+			printf(" 高すぎる　");
+		else
+			printf(" 低すぎる　");
+	}
+
+	if (i <= 10) printf("\n\n%d回目で当たり\n", i);
+	else printf("\n\nアウト!!正解は%dでした\n", n1);
+}
